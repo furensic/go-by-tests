2eb62340133929810d0474c8521f40c465c89370
Leo <info@furensic.eu>
2025-07-02 23:28:27 +0200

    added the iteration function and test
    Signed-off-by: Leo <info@furensic.eu>

---
+++ iteration/iteration_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestIteration(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("got %q, want %q", repeated, expected)
+	}
+}
