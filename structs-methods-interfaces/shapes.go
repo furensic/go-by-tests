0daedf97c803b92f12ea454b723b78263c8da1d4
Leo <info@furensic.eu>
2025-07-03 01:15:48 +0200

    - added structs for Circle and Rectangle shape - added method Area for both circle and rectangle shape
    - refactored test and added test case for circle shape
    
    Signed-off-by: Leo <info@furensic.eu>

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,9 +1,23 @@
 package structs_methods_interfaces
 
-func Perimeter(x, y float64) float64 {
-	return 2 * (x + y)
+import "math"
+
+type Rectangle struct {
+	Width, Height float64
 }
 
-func Area(x, y float64) float64 {
-	return x * y
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * (math.Pow(c.Radius, 2))
 }
