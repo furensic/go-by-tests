a53b6d59116308dee8676d5a79cd38fa0902c708
Leo <info@furensic.eu>
2025-07-03 00:38:54 +0200

    - added the SumAllTails function that makes use of slice indexing 1: to get the sum of all elements in a slice except the 0th element
    Signed-off-by: Leo <info@furensic.eu>

--- array-and-slices/sum.go
+++ array-and-slices/sum.go
@@ -18,3 +18,13 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	numbersOfSlices := len(numbersToSum)
+	sum := make([]int, numbersOfSlices)
+	for i, numbers := range numbersToSum {
+		sum[i] = Sum(numbers[1:])
+	}
+
+	return sum
+}
