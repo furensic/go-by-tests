a0d52341d79f3201942ba46a24887a2e43a9b075
Leo <info@furensic.eu>
2025-07-02 21:38:07 +0200

    - Moved the static string from println into a function returning the string - created a test for the new function

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func ReturnHello() string {
+	return "Hello, World!"
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(ReturnHello())
 }
--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := ReturnHello()
+	want := "Hello, World!"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
