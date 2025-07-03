5629e945d8df87bf5b222b24429edab486dc9a05
Leo <info@furensic.eu>
2025-07-03 14:47:32 +0200

    refactored a bit
    Signed-off-by: Leo <info@furensic.eu>

--- mocking/main.go
+++ mocking/main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
-		fmt.Fprintln(out, i)
+	for i := countdownStart; i > 0; i-- {
+		_, _ = fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Fprint(out, "Go!")
+	_, _ = fmt.Fprint(out, finalWord)
 }
 
 func main() {
