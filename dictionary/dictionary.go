c6877b9a8cd13b37212aa692b3f45c73f499651f
Leo <info@furensic.eu>
2025-07-03 13:15:14 +0200

    implemented error if key wasnt found
    Signed-off-by: Leo <info@furensic.eu>

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,13 @@
 package dictionary
 
+import "fmt"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string, error) {
+	if d[key] == "" {
+		return "", fmt.Errorf("could not find the word")
+	}
+
+	return d[key], nil
 }
